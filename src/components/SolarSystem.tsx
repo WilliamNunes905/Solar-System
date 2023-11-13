43f17bad7682164ba8f0171c9d2d6226281edab1
WilliamNunes905 <williamtrybe@gmail.com>
2023-11-13 11:18:34 -0300

    Rendering the list of planets

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -1,9 +1,20 @@
 import Title from './ Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 function SolarSystem() {
   return (
     <div data-testid="solar-system">
       <Title headline="PLANETAS" />
+      {
+        planets.map(({ name, image }) => (
+          <PlanetCard
+            key={ name }
+            planetImage={ image }
+            planetName={ name }
+          />
+        ))
+      }
     </div>
   );
 }
