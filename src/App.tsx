d50f81e03c7b8d0b884d3c2ab56282e87dfa56fa
WilliamNunes905 <williamtrybe@gmail.com>
2023-11-13 10:17:04 -0300

    Created components Title and SolarSystem

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 import './App.css';
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <SolarSystem />
+    </div>
   );
 }
 
